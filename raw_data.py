13f07c55b15c11d6de7c5f7cb53812f46d8babe6
Alan Cao <alannesta@sina.com>
2017-02-21 08:46:49 -0500

    update data parsing

--- raw_data.py
+++ raw_data.py
@@ -1,18 +1,23 @@
 import re;
 
-f = open('.data/data.sql')
-raw = open('.data/raw', 'w')
+f = open('./data/data.sql')
+raw = open('./data/raw', 'w')
 
 pattern = re.compile("\(\d+,\'(.*?)\'")
 # pattern = re.compile(u"[\u4e00-\u9fa5]+")
 
+result = []
+jointResult = ''
+
 try:
     for line in f:
         result = re.findall(pattern, line)
+        if (len(result) > 0):
+            jointResult = jointResult + '\n'.join(result)
 finally:
     f.close()
 
-jointResult = '\n'.join(result)
+# jointResult = jointResult + '\n'.join(result)
 raw.write(jointResult)
 raw.close()
 
